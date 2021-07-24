051d401ad083742c7512947d6c93c0c06f4c2d36
Parikshit5504 <desaip932@gmail.com>
2021-07-25 00:34:09 +0530

    first commit

---
+++ demo.ts
@@ -0,0 +1,122 @@
+console.log("Welcome toTypeScript !!");
+
+// <--example 1->
+
+// function addNumbers(a: number, b: number) {
+//     return a + b;
+//     }
+    
+//     var sum: number = addNumbers(10,10)
+//     console.log('Sum of a & b is: ' + sum);
+
+// <--showmsg-->
+
+// function showMsg() {
+//     return 'HelloWorld';
+//     }
+    
+//     var i : string = showMsg()
+//     console.log(i);
+
+// <--example 2-->
+
+// let num:number = 1; 
+// let Num:number = 2; 
+// let NUM:number = 3; 
+// let NuM:number = 4; 
+
+// NuM = 40; 
+
+// console.log(NuM)
+
+// <---example 3--->
+
+// let i: number = 987; 
+
+// console.log(i);
+
+// console.log("Welcome toTypeScript !!");
+
+// <--exaample 4-->
+
+// let str: string = 'hello';
+
+// str = 'world';
+
+// str = 987;
+
+// <--example 5-->
+
+// interface ICalc {
+//     add(first: number, second: number): any;
+//     }
+    
+//     let calculator: ICalc = {
+//     add(first: number, second: number) {
+//     return first + second;
+//     }
+//     }
+    
+//     console.log(calculator.add(30,30))
+
+// <--example 6-->
+
+// var i: number = 10;
+// var j: number = 10;
+
+// let msg: string = `Value of i is ${ i } andValue of j is ${ j }`;
+
+// console.log(msg);
+
+// <--example 7-->
+
+// let x = 10;
+// let y = 20;
+
+// let sum = x + y;
+// let sub = x - y;
+// let mul = x * y;
+// let div = x / y;
+
+// console.log('Sum is ', sum);
+// console.log('Sub is ', sub);
+// console.log('Mul is ', mul);
+// console.log('Div is ', div);
+
+// <--example 8-->
+
+// let firstVar = true;
+// let secondVar = false;
+
+// console.log( firstVar && secondVar ); 
+// console.log( firstVar && true );
+
+// <--example 9_-->
+
+// let i = [10, 20, 30];
+
+// for (let value of i) {
+// console.log(value);
+// }
+
+// <--example 10-->
+
+// for (let i = 0; i < 5; i++) {
+
+//     console.log ("No" + i);
+    
+//     }
+
+// <--example  10-->
+
+// var x: number = 10;
+// var y: number = 20;
+
+// if (x == y)
+// {
+// console.log('Value match.');
+// }
+// else {
+// console.log('Value not match.');
+// }
+
